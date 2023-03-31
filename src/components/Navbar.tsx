d5b07771477061fb53246670ccc35ff443ef46f5
Vítor Arimatéa <vitorodev@outlook.com>
2023-03-30 21:37:03 -0300

    Criando sistema de pesquisa

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,39 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 import "../index.css";
 import styles from "../styles";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    
+    if(!search) return;
+
+    navigate(`/search?q=${search}`, { replace: true });
+    setSearch("");
+  };
+
   return (
-    <div>
-        <nav id="navbar" className={`${styles.paddingX} py-[10px] flex items-center justify-between bg-stone-700 text-secondary`}>
-          <h1 className="flex items-center justify-around w-[15%]"> 
-            <BiCameraMovie />
-            <Link to="/"> Apenas Filmes</Link>
+    <nav id="navbar" className={`${styles.paddingX} py-[10px] flex items-center justify-between bg-stone-800 text-secondary font-poppins`}>
+      <h1 className="flex items-center justify-around w-[20%]"> 
+      <div className="text-5xl text-emerald-400">
+        <BiCameraMovie />
+            </div>
+            <Link to="/" className="text-emerald-500 text-xl font-semibold"> Apenas Filmes</Link>
           </h1>
-          <form>
-            <input type="text" placeholder="Buscar filme" className="bg-transparent outline-none" />
-            <button type="submit">
-              <BiSearchAlt2 />
-            </button>
-          </form>
-        </nav>
-    </div>
+          <form onSubmit={handleSubmit} className="flex items-center justify-between">
+            <input type="text" placeholder="Buscar um filme" onChange={(e) => setSearch(e.target.value)} value={search} className="w-[20rem] outline-none rounded-lg p-[3px] text-primary" />
+          <button type="submit" className="text-xl border-[2px] border-emerald-500 bg-emerald-500 p-[3px] ml-5 rounded-lg hover:bg-transparent duration-150">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
   );
-};
+};  
 
 export default Navbar;
